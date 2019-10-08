8302c0260a98ab0cfdb52bce2d75423ee8550d1a
msoft-HoshinoHitoshi <hoshino_hts@bp.msoft.co.jp>
2019-10-08 15:01:53 +0900

    サンプルプロジェクト追加

---
+++ dist/precache-manifest.b0d225ec17abaa1d3b66beaa5e7a61b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "vue-pwa-sample/robots.txt"
+  },
+  {
+    "revision": "46598b5d2a22149cb320",
+    "url": "vue-pwa-sample/js/chunk-vendors.e78714b1.js"
+  },
+  {
+    "revision": "c44bc308199a4e48375c",
+    "url": "vue-pwa-sample/js/app.8959e136.js"
+  },
+  {
+    "revision": "5286171be7296b90d974",
+    "url": "vue-pwa-sample/js/about.c0f25113.js"
+  },
+  {
+    "revision": "95a503ad4e7363dfb6bb0141fd9d1f12",
+    "url": "vue-pwa-sample/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "vue-pwa-sample/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "c44bc308199a4e48375c",
+    "url": "vue-pwa-sample/css/app.dde1df84.css"
+  }
+];
